2f941a2ac987cf108556e02c24e23c77162f1559
Stanislav Zhelnio <zhelniosl@yandex.ru>
2017-11-11 21:14:08 +0300

    05_refactoring refactoring

--- lab_9/src/05_refactoring/pmod_als.v
+++ lab_9/src/05_refactoring/pmod_als.v
@@ -13,8 +13,8 @@ module pmod_als
 (
     input             clk,
     input             rst_n,
-    output reg        cs,
-    output reg        sck,
+    output            cs,
+    output            sck,
     input             sdo,
     output      [7:0] value
 );
@@ -31,73 +31,63 @@ module pmod_als
 
     // sck clock devider
     wire sck_edge;
-    wire sck_out;
     sck_clk_devider scd
     (
         .clk        ( clk      ),
         .rst_n      ( rst_n    ),
-        .sck        ( sck_out  ),
+        .sck        ( sck      ),
         .sck_edge   ( sck_edge )
     );
 
     // State hold registers
     wire [1:0] State;
     reg  [1:0] Next;
-    register #(.SIZE(2)) r_state(clk, rst_n, Next, State);
+    register_we #(.SIZE(2)) r_state(clk, rst_n, sck_edge, Next, State);
 
     wire [23:0] cnt;
     reg  [23:0] cntNext;
-    register #(.SIZE(24)) r_cnt(clk, rst_n, cntNext, cnt);
+    register_we #(.SIZE(24)) r_cnt(clk, rst_n, sck_edge, cntNext, cnt);
 
     wire [7:0] buffer;
     reg  [7:0] bufferNext;
-    register #(.SIZE(8)) r_buffer(clk, rst_n, bufferNext, buffer);
+    register_we #(.SIZE(8)) r_buffer(clk, rst_n, sck_edge, bufferNext, buffer);
 
     reg  [7:0] valueNext;
-    register #(.SIZE(8)) r_value(clk, rst_n, valueNext, value);
+    register_we #(.SIZE(8)) r_value(clk, rst_n, sck_edge, valueNext, value);
 
     // Next state determining
     always @(*) begin
         Next = State;
-        if(sck_edge)
-            case(State)
-                S_IDLE    : if(cnt == IDLE_SIZE)    Next = S_PREFIX;
-                S_PREFIX  : if(cnt == PREFIX_SIZE)  Next = S_DATA;
-                S_DATA    : if(cnt == DATA_SIZE)    Next = S_POSTFIX;
-                S_POSTFIX : if(cnt == POSTFIX_SIZE) Next = S_IDLE;
-            endcase
+        case(State)
+            S_IDLE    : if(cnt == IDLE_SIZE)    Next = S_PREFIX;
+            S_PREFIX  : if(cnt == PREFIX_SIZE)  Next = S_DATA;
+            S_DATA    : if(cnt == DATA_SIZE)    Next = S_POSTFIX;
+            S_POSTFIX : if(cnt == POSTFIX_SIZE) Next = S_IDLE;
+        endcase
     end
 
     always @(*) begin
-        cntNext = cnt;
-        if(sck_edge)
-            case(State)
-                S_IDLE    : cntNext = (cnt == IDLE_SIZE)    ? 0 : cnt + 1;
-                S_PREFIX  : cntNext = (cnt == PREFIX_SIZE)  ? 0 : cnt + 1;
-                S_DATA    : cntNext = (cnt == DATA_SIZE)    ? 0 : cnt + 1;
-                S_POSTFIX : cntNext = (cnt == POSTFIX_SIZE) ? 0 : cnt + 1;
-            endcase
+        cntNext = cnt + 1;
+        case(State)
+            S_IDLE    : if (cnt == IDLE_SIZE)    cntNext = 0;
+            S_PREFIX  : if (cnt == PREFIX_SIZE)  cntNext = 0;
+            S_DATA    : if (cnt == DATA_SIZE)    cntNext = 0;
+            S_POSTFIX : if (cnt == POSTFIX_SIZE) cntNext = 0;
+        endcase
     end
 
     always @(*) begin
         bufferNext = buffer;
         valueNext = value;
-        if(sck_edge)
-            case(State)
-                S_DATA    : bufferNext = { bufferNext[6:0], sdo };
-                S_POSTFIX : valueNext = bufferNext;
-            endcase
-    end
-
-    // output
-    always @(*) begin
         case(State)
-            S_IDLE    : begin cs = 1'b1; sck = 1'b0;    end
-            S_PREFIX  : begin cs = 1'b0; sck = sck_out; end
-            S_DATA    : begin cs = 1'b0; sck = sck_out; end
-            S_POSTFIX : begin cs = 1'b0; sck = sck_out; end
+            S_DATA    : bufferNext = { bufferNext[6:0], sdo };
+            S_POSTFIX : valueNext = bufferNext;
         endcase
     end
+
+    // buffered output
+    wire csNext = (Next == S_IDLE);
+    register_we #(.SIZE(1)) r_cs(clk, rst_n, sck_edge, csNext, cs);
 
 endmodule
 
