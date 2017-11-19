d2d21c6aa64a891de01afb8b0a37c232c1c92faf
yuri.panchul <yuri@panchul.com>
2017-11-19 13:16:46 -0800

    Changing the structure of Lab 10 source

---
+++ lab_10/src/04_syn_pow_5_en_multi_cycle_struct/pow_5_en_multi_cycle_struct.v
@@ -0,0 +1,79 @@
+module pow_5_en_multi_cycle_struct
+# (
+    parameter w = 8
+)
+(
+    input            clk,
+    input            rst_n,
+    input            clk_en,
+    input            n_vld,
+    input  [w - 1:0] n,
+    output           res_vld,
+    output [w - 1:0] res
+);
+
+    wire           n_vld_q;
+    wire [w - 1:0] n_q;
+
+    reg_rst_n_en        i_n_vld (clk, rst_n, clk_en, n_vld, n_vld_q);
+    reg_no_rst_en # (w) i_n     (clk, clk_en, n, n_q);
+
+    wire [4:0] shift_q;
+    wire [4:0] shift_d = n_vld_q ? 5'b10000 : shift_q >> 1;
+   
+    reg_rst_n_en # (5) i_shift (clk, rst_n, clk_en, shift_d, shift_q);
+    
+    assign res_vld = shift_q [0];
+
+    wire [w - 1:0] mul_q;
+    wire [w - 1:0] mul_d = n_vld_q ? n_q : mul_q * n_q;
+
+    wire mul_en = clk_en;  // && (n_vld_q || shift_q [4:1] != 4'b0);
+
+    reg_no_rst_en # (w) i_mul (clk, mul_en, mul_d, mul_q);
+    
+    assign res = mul_q;
+
+endmodule
+
+//----------------------------------------------------------------------------
+
+`ifndef SIMULATION
+
+module top
+(
+    input         fast_clk,
+    input         slow_clk,
+    input         rst_n,
+    input         fast_clk_en,
+    input  [ 3:0] key,
+    input  [ 7:0] sw,
+    output [ 7:0] led,
+    output [ 7:0] disp_en,
+    output [31:0] disp,
+    output [ 7:0] disp_dot
+);
+
+    wire res_vld;
+    
+    assign led  = { 8 { res_vld } };
+
+    pow_5_en_multi_cycle_struct
+    # (.w (8))
+    i_pow_5_en
+    (
+        .clk     ( fast_clk    ),
+        .rst_n   ( rst_n       ),
+        .clk_en  ( fast_clk_en ),
+        .n_vld   ( key [0]     ),
+        .n       ( sw          ),
+        .res_vld ( res_vld     ),
+        .res     ( disp [7:0]  )
+    );
+    
+    assign disp_en  = 8'b00000011;
+    assign disp_dot = { 7'b0000000, res_vld };
+
+endmodule
+
+`endif
---
+++ lab_10/src_old/08_syn_pow_5_en_pipe_always/top.v
@@ -0,0 +1,101 @@
+module pow_5_en_pipe_always
+# (
+    parameter w = 8
+)
+(
+    input                clk,
+    input                rst_n,
+    input                clk_en,
+    input                n_vld,
+    input  [w     - 1:0] n,
+    output [        3:0] res_vld,
+    output [w * 4 - 1:0] res
+);
+
+    reg [w - 1:0] n1, n2, n3, n4;
+    reg [w - 1:0] pow2, pow3, pow4, pow5;
+    reg n_vld_1, n_vld_2, n_vld_3, n_vld_4, n_vld_5;
+
+    always @ (posedge clk or negedge rst_n)
+        if (! rst_n)
+        begin
+            n_vld_1 <= 1'b0;
+            n_vld_2 <= 1'b0;
+            n_vld_3 <= 1'b0;
+            n_vld_4 <= 1'b0;
+            n_vld_5 <= 1'b0;
+        end
+        else if (clk_en)
+        begin
+            n_vld_1 <= n_vld;
+            n_vld_2 <= n_vld_1;
+            n_vld_3 <= n_vld_2;
+            n_vld_4 <= n_vld_3;
+            n_vld_5 <= n_vld_4;
+        end
+
+    always @ (posedge clk)
+        if (clk_en)
+        begin
+            n1 <= n;
+            n2 <= n1;
+            n3 <= n2;
+            n4 <= n3;
+
+            pow2 <= n1 * n1;
+            pow3 <= pow2 * n2;
+            pow4 <= pow3 * n3;
+            pow5 <= pow4 * n4;
+        end
+
+    assign res_vld = { n_vld_2 , n_vld_3 , n_vld_4 , n_vld_5 };
+    assign res     = { pow2    , pow3    , pow4    , pow5    };
+
+endmodule
+
+//--------------------------------------------------------------------
+
+`ifndef SIMULATION
+
+module top
+(
+    input         fast_clk,
+    input         slow_clk,
+    input         rst_n,
+    input         fast_clk_en,
+    input  [ 3:0] key,
+    input  [ 7:0] sw,
+    output [ 7:0] led,
+    output [ 7:0] disp_en,
+    output [31:0] disp,
+    output [ 7:0] disp_dot
+);
+
+    wire [3:0] res_vld;
+
+    pow_5_en_pipe_always
+    # (.w (8))
+    i_pow_5_en
+    (
+        .clk     ( fast_clk    ),
+        .rst_n   ( rst_n       ),
+        .clk_en  ( fast_clk_en ),
+        .n_vld   ( key [0]     ),
+        .n       ( sw          ),
+        .res_vld ( res_vld     ),
+        .res     ( disp [31:0] )
+    );
+
+    assign disp_en  =
+    {
+        res_vld [3], res_vld [3],
+        res_vld [2], res_vld [2],
+        res_vld [1], res_vld [1],
+        res_vld [0], res_vld [0]
+    };
+
+    assign disp_dot = 8'b0;
+
+endmodule
+
+`endif
